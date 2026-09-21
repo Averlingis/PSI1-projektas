e1dd63d7a23bd51120d553a225d57b9c65d4a7a8
eggr-png <edgar.dainarovic@gmail.com>
2026-09-21 22:00:28 +0300

    removed code repetition in language selection request

--- src/PSI1.Api/DTOs/LanguageSelectionRequest.cs
+++ src/PSI1.Api/DTOs/LanguageSelectionRequest.cs
@@ -1,6 +1,5 @@
-using System.Text.Json.Serialization;
 using PSI1.Api.Models;
 
 namespace PSI1.Api.DTOs;
 
-public record LanguageSelectionRequest([property: JsonConverter(typeof(JsonStringEnumConverter))] Language Language);
+public record LanguageSelectionRequest(Language Language);
