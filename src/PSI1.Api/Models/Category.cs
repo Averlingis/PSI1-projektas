d09749d0e36a366d709ff46d9ccd39d729c31358
Edvinas Averlingis <edvinasaverlingis123@gmail.com>
2026-09-23 16:01:44 +0300

    Feature/select category (#49)
    * Add Category enum with initial set of learning categories
    
    * Add category selection endpoints and tests
    
    ---------
    
    Co-authored-by: Edvinas Averlingis <edvinas.averlingis@mif.stud.vu.lt>

---
+++ src/PSI1.Api/Models/Category.cs
@@ -0,0 +1,17 @@
+namespace PSI1.Api.Models;
+
+public enum Category
+{
+    Nature,
+    Culture,
+    Food,
+    Travel,
+    Work,
+    Health,
+    Home,
+    Shopping,
+    Education,
+    Technology,
+    Sports,
+    Family
+}
